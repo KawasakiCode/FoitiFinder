538756e0728991766bf4c549882771501f228505
Kawasaki <remvosp@gmail.com>
2025-11-08 19:14:54 +0200

    removed unused platform folders and changed project name in multiplefiles

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:anapp2/main.dart';
+import 'package:foitifinder/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
