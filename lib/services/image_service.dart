0773649ceb67bfee3b0ee9081c5ec9277f5fdfd3
Kawasaki <remvosp@gmail.com>
2025-12-17 18:26:11 +0200

    forgoten to push file that handles image loading for provider

---
+++ lib/services/image_service.dart
@@ -0,0 +1,45 @@
+//manages local profile image handling as well as picking an image from users gallery
+//gets called strictly by the profile_provider.dart to handle pfp
+
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class ImageService {
+  //the object that handles grabbing the file that the user selects
+  static final ImagePicker _picker = ImagePicker();
+
+  //the function that handles the picking and also reducing the image quality
+  //to save size and upload times
+  //Returns the path of the selected image
+  static Future<File?> pickImage() async {
+    final XFile? pickedFile = await _picker.pickImage(  
+      source: ImageSource.gallery,
+      imageQuality: 80,
+      maxWidth: 1000,
+      maxHeight: 1000,
+    );
+
+    if(pickedFile == null)return null;
+    return File(pickedFile.path);
+  }
+
+  //function that uploads the image file in the firebase cloud and returns the unique url for it
+  //image is the image that will be uploaded to the cloud
+  //uid is the unique user identifier so postgres knows to which user to save the returned url
+  static Future<String?> uploadImage(File image, String uid) async {
+    try {
+      //the adress in the cloud. Where the file will be stored in the cloud bucket
+      final Reference storageRef = FirebaseStorage.instance
+      .ref().child('profile_image/$uid.jpg');
+
+      //upload of the file to the cloud
+      final UploadTask uploadTask = storageRef.putFile(image);
+      //pause until the file gets uploaded
+      final TaskSnapshot snapshot = await uploadTask;
+      return await snapshot.ref.getDownloadURL();
+    } catch (e) {
+      return null;
+    }
+  }
+}
